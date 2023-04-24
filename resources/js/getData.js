e31c892bf9605246c98d09539c4134de0bc1c6ca
Aalaizha B <l.bnjamin0309@gmail.com>
2023-04-24 18:30:52 -0400

    IMPROVE: Convert response obj to JSON format

--- resources/js/getData.js
+++ resources/js/getData.js
@@ -7,8 +7,10 @@ export async function getAPI() {
         const url = `https://api.nytimes.com/svc/books/v3/lists/${createFullDate()}/hardcover-fiction.json?api-key=0rqNBm0BPfy1GAapCh5ICHcOebDlV76o`
 
         const response = await fetch(url);
-
-        console.log(response)
+        //console.log(response)
+        
+        const responseObj = await response.json()
+        console.log(responseObj)
    } catch(err) {
         console.error(err)
    }
