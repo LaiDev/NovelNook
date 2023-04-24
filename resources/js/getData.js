e8705dd42ef7fa66f9613c8469a370b2882102eb
Aalaizha B <l.bnjamin0309@gmail.com>
2023-04-24 17:39:08 -0400

    ADD: Function to get a random year

--- resources/js/getData.js
+++ resources/js/getData.js
@@ -1,3 +1,11 @@
 const createFullDate = () => {
-    
+
+//Gets a year between 2009 and the current year
+
+const getRandomYear = () =>{
+    const currentYear = new Date().getFullYear();
+    const randomYear = Math.floor(Math.random() * (currentYear - 2009 + 1) + 2009 )
+    return randomYear
 }
+
+};
