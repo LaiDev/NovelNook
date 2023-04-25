e69aea94bae5af9c4c9b2b7ceb802e723f6c20ca
Aalaizha B <l.bnjamin0309@gmail.com>
2023-04-25 11:39:31 -0400

    ADD: Get 5 random books

--- resources/js/getData.js
+++ resources/js/getData.js
@@ -10,7 +10,20 @@ export async function getAPI() {
         //console.log(response)
         
         const responseObj = await response.json()
-        console.log(responseObj)
+        const booksArr = await responseObj.results.books
+
+        //Gets 5 random books from the array
+        for(let i = 0; i < 5; i++)
+        {
+          let randomNum = Math.floor(Math.random() * booksArr.length)
+          console.log(booksArr[randomNum])
+          let bookDescription = booksArr[randomNum].description;
+          let bookTitle = booksArr[randomNum].title;
+          let bookauthor = booksArr[randomNum].author;
+          console.log(`The book is ${bookTitle} written by ${bookauthor}. It is about ${bookDescription}`)
+
+        }
+
    } catch(err) {
         console.error(err)
    }
