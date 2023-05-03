d5d79d61de24f93ff2a3c08923b42571f0f27167
Aalaizha B <l.bnjamin0309@gmail.com>
2023-05-03 17:45:14 -0400

    IMPROVE: Nav page indicator

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -1,11 +1,10 @@
-const activePage = window.location.pathname;
+const activePage = window.location.href;
+console.log(activePage)
 //Loops through all the nav links
 const navLinks = document.querySelectorAll("nav a").forEach(link => {
     if(link.href.includes(`${activePage}`)){
+        console.log(link)
         //If the current link matches the current page, add the active class
         link.classList.add("active")
-    } else
-    {
-        link.classList.remove("active")
     }
 })
