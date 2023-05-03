16895d2a44ec04d4c4e5d6ae5408658fae0eea99
Aalaizha B <l.bnjamin0309@gmail.com>
2023-05-03 14:54:07 -0400

    ADD: Functionality to indicate active page

---
+++ resources/js/nav.js
@@ -0,0 +1,8 @@
+const activePage = window.location.pathname;
+//Loops through all the nav links
+const navLinks = document.querySelectorAll("nav a").forEach(link => {
+    if(link.href.includes(`${activePage}`)){
+        //If the current link matches the current page, add the active class
+        link.classList.add("active")
+    }
+})
