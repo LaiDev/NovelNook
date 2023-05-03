5b1058168fb7d4e6e93b536c6d1752ee4ac665a5
Aalaizha B <l.bnjamin0309@gmail.com>
2023-05-03 15:02:12 -0400

    IMPROVE: Nav Indicator on hover

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -4,5 +4,8 @@ const navLinks = document.querySelectorAll("nav a").forEach(link => {
     if(link.href.includes(`${activePage}`)){
         //If the current link matches the current page, add the active class
         link.classList.add("active")
+    } else
+    {
+        link.classList.remove("active")
     }
 })
