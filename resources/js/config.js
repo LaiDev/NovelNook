968db81841cf403413175da37c6a473b4aae6230
Aalaizha B <l.bnjamin0309@gmail.com>
2023-05-30 12:30:46 -0400

    ADD: URL

--- resources/js/config.js
+++ resources/js/config.js
@@ -2,6 +2,8 @@ let config = {
 
 SECRET_API_KEY: "0rqNBm0BPfy1GAapCh5ICHcOebDlV76o",
 
-SUPA_API_KEY : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkbHNjc2NxcHhxbHd5Z2xrcWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MTI0MjgsImV4cCI6MjAwMDM4ODQyOH0._tzA7hs32zzwvZUwX9e5du-TdL0zqR01VBV8egJun9k"
+SUPA_API_KEY : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrbGhjZm12cmtvb254eWhub2FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MTYwMTgsImV4cCI6MjAwMDM5MjAxOH0.X1mjQV34ZfNqyG_op20Ye_r5PWk-yqFcSjdHmW4Ln1U",
+
+SUPA_URL: "https://eklhcfmvrkoonxyhnoas.supabase.co"
 
 }
